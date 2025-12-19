df92703d46289b341496086b82028ede5baf75b1
Jannik Wiest <jannik.wiest@gmail.com>
2025-12-20 00:22:55 +0100

    reset max simulation speed to 2000HZ

--- src/UI.cpp
+++ src/UI.cpp
@@ -61,9 +61,9 @@ void UI::loadFont() {
 }
 
 void UI::updateSimSpeed() {
-  // Map slider value (0.0-1.0) to speed (0.5 - 4000 Hz)
+  // Map slider value (0.0-1.0) to speed (0.5 - 2000 Hz)
   // Use exponential scale for better control at low speeds
-  simSpeed_ = 0.5f * std::pow(8000.0f, sliderValue_);
+  simSpeed_ = 0.5f * std::pow(4000.0f, sliderValue_);
 }
 
 void UI::updateLayout(const sf::Vector2u& windowSize) {
